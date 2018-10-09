a5c22073205a6b1fdf45ff0b71a0159adc96e7d1
yamashita yasunari <yyyamashita8@gmail.com>
2018-10-09 14:53:38 +0900

    serviceWorker test

--- service_worker.js
+++ service_worker.js
@@ -1,10 +1,10 @@
 //service_worker.js
-self.addEventListener('install', function(e)){
+self.addEventListener('install', function(e){
   console.log('[serviceWorker] Install');
-}
+});
 
-self.addEventListener('activate', function(e)){
+self.addEventListener('activate', function(e){
   console.log('[serviceWorker] Activate');
-}
+});
 
 self.addEventListener('fetch', function(event){});
